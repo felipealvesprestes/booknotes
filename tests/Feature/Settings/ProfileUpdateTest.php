9c5630eaec76dcc64aeada20a31b083a7b5f51dc
Felipe Prestes <felipealvesprestes@gmail.com>
2025-11-22 19:07:58 -0300

    test: ajusta testes falhos

--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -20,6 +20,13 @@ test('profile information can be updated', function () {
     $response = Livewire::test(Profile::class)
         ->set('name', 'Test User')
         ->set('email', 'test@example.com')
+        ->set('cep', '90570-050')
+        ->set('cpf', '39053344705')
+        ->set('address_street', 'Rua de Teste')
+        ->set('address_number', '123')
+        ->set('address_neighborhood', 'Centro')
+        ->set('address_city', 'Porto Alegre')
+        ->set('address_state', 'RS')
         ->call('updateProfileInformation');
 
     $response->assertHasNoErrors();
@@ -28,17 +35,27 @@ test('profile information can be updated', function () {
 
     expect($user->name)->toEqual('Test User');
     expect($user->email)->toEqual('test@example.com');
+
     expect($user->email_verified_at)->toBeNull();
 });
 
 test('email verification status is unchanged when email address is unchanged', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
 
     $this->actingAs($user);
 
     $response = Livewire::test(Profile::class)
         ->set('name', 'Test User')
         ->set('email', $user->email)
+        ->set('cep', '12345-678')
+        ->set('cpf', '12345678909')
+        ->set('address_street', 'Rua de Teste')
+        ->set('address_number', '123')
+        ->set('address_neighborhood', 'Centro')
+        ->set('address_city', 'Porto Alegre')
+        ->set('address_state', 'RS')
         ->call('updateProfileInformation');
 
     $response->assertHasNoErrors();
