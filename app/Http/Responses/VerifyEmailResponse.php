747d6f90754f3cb1cf6715f0849005633f1c9059
Felipe Prestes <felipealvesprestes@gmail.com>
2025-11-27 13:26:09 -0300

    fix: ajusta link verificação cadastro

---
+++ app/Http/Responses/VerifyEmailResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\JsonResponse;
+use Laravel\Fortify\Contracts\VerifyEmailResponse as VerifyEmailResponseContract;
+use Laravel\Fortify\Fortify;
+
+class VerifyEmailResponse implements VerifyEmailResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     */
+    public function toResponse($request)
+    {
+        $verificationRedirect = Fortify::redirects('email-verification').'?verified=1';
+
+        return $request->wantsJson()
+            ? new JsonResponse('', 204)
+            : redirect()->to($verificationRedirect);
+    }
+}
--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -4,11 +4,13 @@ namespace App\Providers;
 
 use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
+use App\Http\Responses\VerifyEmailResponse as CustomVerifyEmailResponse;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Laravel\Fortify\Contracts\VerifyEmailResponse as VerifyEmailResponseContract;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -18,7 +20,10 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            VerifyEmailResponseContract::class,
+            CustomVerifyEmailResponse::class
+        );
     }
 
     /**
