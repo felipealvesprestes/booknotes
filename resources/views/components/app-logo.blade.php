9f37ba0df0b58454d4e98bf6675b999222fd0038
Felipe Prestes <felipealvesprestes@gmail.com>
2025-12-05 08:20:13 -0300

    refac: ajusta cor links menu e tamanho logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,5 +1,5 @@
-<div class="flex aspect-square size-9 items-center justify-center">
-    <x-app-logo-icon class="size-9 text-accent-foreground" />
+<div class="flex aspect-square size-7 items-center justify-center">
+    <x-app-logo-icon class="size-7 text-accent-foreground" />
 </div>
 <div class="grid flex-1 text-start leading-tight">
     <span class="mb-0.5 truncate text-sm font-bold tracking-tight">BOOK<span class="text-emerald-500">NOTES</span></span>
